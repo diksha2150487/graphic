498481ee70ba154f3142697b7f6ed226915fc195
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-07-25 14:06:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,191 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import img1 from "./images/Group 1000006883.png";
+import img2 from "./images/4652 1.png";
+import img3 from "./images/Rectangle 18187.png";
+import img4 from "./images/Rectangle 18188.png";
+import img5 from "./images/Rectangle 18189.png";
+import img6 from "./images/Rectangle 18187 (1).png";
+import img7 from "./images/Rectangle 18188 (1).png";
+import img8 from "./images/Rectangle 18189 (1).png";
+import img9 from "./images/Rectangle 18187 (2).png";
+import img10 from "./images/Rectangle 18188 (2).png";
+import img11 from "./images/Rectangle 18189 (2).png";
+import img12 from "./images/Rectangle 18187 (3).png";
+import img13 from "./images/Rectangle 18188 (3).png";
+import img14 from "./images/Rectangle 18189 (3).png";
+function App() {
+  return (
+    <div>
+      <div className="paper1">
+        <div className="leftpaper1">
+          <p className="Ambitious">Design Services for Ambitious Brands</p>
+          <div className="line-single">
+            <div class="vl"></div>
+            <p className="brand">
+              Teams at fast-growing companies use Superside to get quality
+              graphic design done at scale. Book a call today and get acess to
+              your dedicated design team.
+            </p>
+          </div>
+          <button className="team">
+            <p> Consult our Graphic Design Team</p>{" "}
+            <AiOutlineArrowRight id="fArrow" />
+          </button>
+        </div>
+        <div className="rightpaper1">
+          <img src={img1} alt="img" className="three-circle" />
+        </div>
+      </div>
+      {/* ************************************************************** */}
+      <div className="black-main">
+        <div className="left-class-text">
+          <p className="cello">
+            A VARIETY OF DESIGN SERVICES AT YOUR FINGERTIPS
+          </p>
+          <p className="motion">
+            Get graphic design, print, motion, video and more
+          </p>
+          <p className="traditional">
+            Whether you need an out-of-this-world illustration, beautiful print
+            designs, or engaging digital marketing assets, Kushel Digi
+            world-class graphic designers from around the world will make it
+            happen.
+          </p>
+          <p className="model">
+            Unlike traditional graphic design agencies, Kushel Digi is a tech
+            enabled, Creative-as-a-Service design solution built to keep up with
+            the evolving needs and fast pace of enterprise teams. Access a full
+            suite of design capabilities through a transparent design
+            subscription model.
+          </p>
+        </div>
+        <div className="right-class-bulb">
+          <img src={img2} id="yellow-bllok"></img>
+        </div>
+      </div>
+      {/* ************************************************************** */}
+      <div className="third-box-design">
+        <p className="canny">Design Services</p>
+        <hr id="short-line" />
+        <div className="main-box">
+          <div className="first-box">
+            <img src={img3} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img4} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img5} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img6} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img7} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img8} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img9} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img10} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img11} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+            <hr className="long-line" />
+          </div>
+          <div className="first-box">
+            <img src={img12} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+          </div>
+          <div className="first-box">
+            <img src={img13} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+          </div>
+          <div className="first-box">
+            <img src={img14} id="first-img"></img>
+            <p className="Ad">Ad Creative</p>
+            <p className="pirates">
+              Get Static and motion ad creative, concepts, and variations for
+              testing your way to better result across social media.{" "}
+            </p>
+          </div>
+        </div>
+        <p className="driving">DRIVING GREAT OUTCOMES</p>
+        <div className="main-button">
+          <p className="as-creative">
+            #1 Creative-as-a-Service solution for marketing and creative teams
+          </p>
+          <button className="read_now">Book a Demo</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
